9d9184337f8e8e1b0a808cbf6a713755d5e0b6ca
Sanaullah M S <SanaullahMS@sulekha.com>
2020-08-13 12:36:01 +0530

    sorting array programs

--- src/com/sanaullah/Main.java
+++ src/com/sanaullah/Main.java
@@ -1,10 +1,18 @@
 package com.sanaullah;
 
+import com.sanaullah.arrays.SortingArray;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Initial Main");
 
+        SortingArray sortingArray = new SortingArray();
 
+        int [] array = {32,3,4,41,23,43,56,1};
+        System.out.println("Array In Ascending Order--->"+Arrays.toString(sortingArray.sortArrayInAscendingOrder(array)));
+        System.out.println("Array In Descending Order--->"+Arrays.toString(sortingArray.sortArrayInDescendingOrder(array)));
     }
 }
---
+++ src/com/sanaullah/arrays/SortingArray.java
@@ -0,0 +1,36 @@
+package com.sanaullah.arrays;
+
+
+
+/**
+ * @author Saanaullah
+ */
+public class SortingArray {
+
+    public int [] sortArrayInAscendingOrder(int [] givenArray){
+        for (int i = 0; i < givenArray.length; i++) {
+            for (int j = 0; j < givenArray.length; j++) {
+                if(givenArray[i] < givenArray[j]){
+                    int temp = givenArray[i];
+                    givenArray[i] = givenArray[j];
+                    givenArray[j] = temp;
+                }
+            }
+        }
+        return givenArray;
+    }
+
+    public int [] sortArrayInDescendingOrder(int [] givenArray){
+        for (int i = 0; i < givenArray.length; i++) {
+            for (int j = 0; j < givenArray.length; j++) {
+                if(givenArray[i] < givenArray[j]){
+
+                    int temp = givenArray[i];
+                    givenArray[i] = givenArray[j];
+                    givenArray[j] = temp;
+                }
+            }
+        }
+        return givenArray;
+    }
+}
